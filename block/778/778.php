fd45bbdfd6a67145512369dd4ea2b0566cfeee1c
kimco2404 <123456789123456789>
2017-03-24 14:14:56 +0700

    updates files

---
+++ block/778/778.php
@@ -0,0 +1,168 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>778</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <?php
+        if (!class_exists('lessc')) {
+            include './libs/lessc.inc.php';
+        }
+        $less = new lessc;
+        $less->compileFile('./less/778.less', './css/778.css')
+        ?>
+
+        <link href="css/responsive.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/778.css" rel="stylesheet" type="text/css"/>
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+
+        <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/xxx.js" type="text/javascript"></script>
+    </head>
+
+    <body>
+
+        <div class="type-778">
+            <div class="pb-fw-wap">
+                <div class="page-wrap">
+                    <div class="page-content">
+                        <div class="inner-page-wrap has-no-sidebar">
+                            <article class="product-tag-levis product-tag-vintage instock">
+                                <div class="page-content">
+                                    <div class="article-body-wrap">
+                                        <div itemscope itemtype="#" class="product instock">
+                          
+                                            <div class="container product-after-wrap">
+                                                <div class="product-carousel spb_content_element">
+                                                    <h4 class="lined-heading">
+                                                        <span>Related Products</span>
+                                                    </h4>
+                                                    <div class="carousel-wrap">
+                                                        <ul class="related products carousel-items owl-carousel owl-theme" id="carousel-1" data-columns="4>" style="margin-left: -55px; width: 1200px; opacity: 1; display: block;">
+                                                            <div class="owl-wrapper-outer">
+                                                                <div class="owl-wrapper" style="width: 1200px; left: 0px; display: block;">
+                                                                    <div class="owl-item" style="width: 300px;">
+                                                                        <li class="product type-productproduct-tag-our-legacy instock">
+                                                                            <figure class="product-transition" style="padding-bottom: 360px;">
+                                                                                <div class="product-img-wrap">
+                                                                                    <div class="product-image">
+                                                                                        <img width="540" height="720" src="images/ol-jeans-jacket-tiger001-540x720.jpg" class="attachment-shop_catalog size-shop_catalog" alt="ol-jeans-jacket-tiger001">
+                                                                                    </div>
+                                                                                    <div class="product-image second-image">
+                                                                                        <img width="540" height="720" src="images/ol-jeans-jacket-tiger007-540x720.jpg" class="attachment-shop_catalog size-shop_catalog" alt="ol-jeans-jacket-tiger007">
+                                                                                    </div>
+                                                                                </div>
+                                                                                <figcaption>
+                                                                                    <div class="shop-actions clearfix">
+                                                                                        <a href="#" rel="nofollow" class="add_to_cart_button ajax_add_to_cart product_type_simple" data-added_text="Item added to bag">
+                                                                                            <i class="icon-plus"></i>Add to shopping bag
+                                                                                        </a>
+                                                                                        <div class="yith-wcwl-add-to-wishlist">
+                                                                                            <div class="yith-wcwl-add-button show">
+                                                                                                <a href="#" data-product-type="simple" class="add_to_wishlist">
+                                                                                                    <i class="fa-star"></i>
+                                                                                                </a>
+                                                                                            </div>
+                                                                                            <div class="yith-wcwl-wishlistaddedbrowse hide" style="display:none;">
+                                                                                                <span class="feedback">Product added to wishlist.</span>
+                                                                                                <a href="#"><i class="fa-check"></i></a>
+                                                                                            </div>
+                                                                                            <div class="yith-wcwl-wishlistexistsbrowse hide" style="display:none">
+                                                                                                <a href="#"><i class="fa-check"></i></a>
+                                                                                            </div>
+                                                                                            <div style="clear:both"></div>
+                                                                                            <div class="yith-wcwl-wishlistaddresponse"></div>
+
+                                                                                        </div>
+                                                                                    </div>
+                                                                                </figcaption>
+                                                                                <a class="product-hover-link"></a>
+                                                                            </figure>
+                                                                            <div class="product-details">
+                                                                                <h3><a href="#">Our Legacy  Jean Jacket</a></h3>
+                                                                                <span class="posted_in"> <a rel="tag">Jackets</a></span>
+                                                                            </div>
+                                                                            <span class="price">
+                                                                                <span class="amount">£170.00</span>
+                                                                            </span>
+                                                                        </li></div><div class="owl-item" style="width: 300px;"><li class="product type-product  product-tag-shirts instock">
+                                                                            <figure class="product-transition" style="padding-bottom: 360px;">
+                                                                                <div class="product-img-wrap">
+                                                                                    <div class="product-image">
+                                                                                        <img width="540" height="720" src="images/ol-jeans-jacket-tiger001-540x720.jpg" class="attachment-shop_catalog size-shop_catalog" alt="ol-jeans-shirt001">
+                                                                                    </div>
+                                                                                    <div class="product-image second-image">
+                                                                                        <img width="540" height="720" src="images/ol-jeans-shirt006-540x720.jpg" class="attachment-shop_catalog size-shop_catalog" alt="ol-jeans-shirt006">
+                                                                                    </div> 
+                                                                                </div>
+                                                                                <figcaption>
+                                                                                    <div class="shop-actions clearfix">
+                                                                                        <a href="#" rel="nofollow" class="add_to_cart_button ajax_add_to_cart product_type_simple" data-added_text="Item added to bag"><i class="icon-plus"></i>Add to shopping bag</a><div class="yith-wcwl-add-to-wishlist"><div class="yith-wcwl-add-button show"><a href="#" data-product-id="9693" data-product-type="simple" class="add_to_wishlist">
+                                                                                                    <i class="fa-star"></i>
+                                                                                                </a>
+                                                                                            </div>
+                                                                                            <div class="yith-wcwl-wishlistaddedbrowse hide" style="display:none;">
+                                                                                                <span class="feedback">Product added to wishlist.</span>
+                                                                                                <a href="#">
+                                                                                                    <i class="fa-check">
+
+                                                                                                    </i>
+                                                                                                </a>
+                                                                                            </div>
+                                                                                            <div class="yith-wcwl-wishlistexistsbrowse hide" style="display:none">
+                                                                                                <a href="#">
+                                                                                                    <i class="fa-check"></i>
+                                                                                                </a>
+                                                                                            </div>
+                                                                                            <div style="clear:both">
+
+                                                                                            </div>
+                                                                                            <div class="yith-wcwl-wishlistaddresponse">
+
+                                                                                            </div>
+
+                                                                                        </div>
+                                                                                    </div>
+                                                                                </figcaption>
+                                                                                <a class="product-hover-link" href="#"></a>
+                                                                            </figure>
+                                                                            <div class="product-details">
+                                                                                <h3><a href="#">Our Legacy  Half Zip Shirt</a></h3>
+                                                                                <span class="posted_in"> <a href="#" rel="tag">Shirts</a></span>
+                                                                            </div>
+                                                                            <span class="price">
+                                                                                <span class="amount">£125.00</span>
+                                                                            </span>
+                                                                        </li
+                                                                        ></div>
+                                                                </div>
+                                                            </div>
+
+                                                        </ul>
+                                                        <a href="#" class="carousel-prev" style="display: none;">
+                                                            <i class="fa-chevron-left"></i>
+                                                        </a>
+                                                        <a href="#" class="carousel-next" style="display: none;">
+                                                            <i class="fa-chevron-right"></i>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </article>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
