aedfa6d373547b81b1b53297b2493bdc75202cb8
NguyenVanLuong <luongdia16@gmail.com>
2017-04-28 02:03:34 +0700

    Commit

--- block/741/js/741.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(".jq-selectbox_select").click(function () {
-        $(".jq-selectbox_dropdown").slideToggle();
-        $(".jq-selectbox_trigger").css("transform", "rotateX(180deg)");
-    });
-});
-
-
